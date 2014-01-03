9dc32ef0cde7136c8a5f769571b0ac447a8b23b5
Joshua Piccari <joshua.piccari@gmail.com>
2014-01-02 23:47:13 -0800

    Swapping strdup in place of malloc + strcpy

--- src/nodes.c
+++ src/nodes.c
@@ -62,8 +62,7 @@ node_new(enum ast_node_type type, ast_node *e1, ast_node *e2, ast_node *e3,
 
   node->sval = NULL;
   if (s != NULL) {
-    node->sval = malloc((strlen(s) + 1) * sizeof(char));
-    strcpy(node->sval, s);
+    node->sval = strdup(s);
   }
   return node;
 }
